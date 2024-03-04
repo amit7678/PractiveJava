d558787327258bf342297a02836f63a640e28fd2
amit <amitmaurya588811@gmail.com>
2024-03-04 14:11:14 +0530

    first commit

---
+++ src/com/p/Learn.java
@@ -0,0 +1,74 @@
+package com.p;
+import java.util.*;
+import java.util.stream.Collectors;
+public class Learn {
+
+	public static void main(String[] args) {
+		
+		List<Integer>l1=Arrays.asList(1,5,4,7,8,9,1,4);
+      System.out.println("unique objects");
+		
+	long count=l1.stream().distinct().count();
+	System.out.println(count);
+		
+		
+		System.out.println("Each number multiply by 3");
+		l1.stream().map(n->n*3).forEach(System.out::println);
+		
+		l1=l1.stream().filter(n->n%2==0).collect(Collectors.toList());
+		System.out.println(l1);
+		
+		System.out.println("unique objects");
+		
+		l1.stream().distinct().forEach(System.out::println);
+		
+		
+       List<String>l2=Arrays.asList("amit","mauryajikaha","kumartumho","ifthe","is");
+     
+    l2= l2.stream().filter(n->n.length()>4 && n.length()<11).collect(Collectors.toList());
+      
+      System.out.println(l2);
+      
+    l2=l2.stream().map(n->n.toUpperCase()).collect(Collectors.toList());
+    System.out.println(l2);
+    
+    l2.stream().map(n->n.length()).forEach(System.out::println);
+    
+    
+    
+    
+    List<Integer>l5=Arrays.asList(10,5,4,7,8,9,18,4);
+    
+    
+    Optional<Integer>min= l5.stream().min((v1,v2)->{return v1.compareTo(v2);});
+    System.out.println(min.get());
+    
+    
+   Optional<Integer>max=l5.stream().max((v1,v2)->{return v1.compareTo(v2);});
+    
+    System.out.println(max.get());
+    
+    
+    List<String>l59=Arrays.asList("A","b","10","5","4","g");
+    Optional<String>kk=l59.stream().reduce((value,combine)->{return combine+value;});
+    System.out.println(kk.get());
+    
+    
+    
+    
+    Set<String>reSet=new HashSet<String>();
+    reSet.add("one apple");
+    reSet.add("one apple");
+    reSet.add("two apple");
+    reSet.add("four guava");
+    reSet.add("five orange");
+    
+    boolean res8=reSet.stream().anyMatch(v->{return v.startsWith("one");});
+    System.out.println(res8);
+    
+    
+    
+    
+	}
+
+}
